1283d2c185c4cab8a66f071d92869bfc0f58c20a
ubuntu <yano@ubuntu.(none)>
2014-03-13 00:50:39 +0900

    refactor WaveletMatrix.hpp

--- waveletMatrix/WaveletMatrix.hpp
+++ waveletMatrix/WaveletMatrix.hpp
@@ -14,7 +14,7 @@ public:
     WaveletMatrix(const std::vector<int>& array);
     int access(const int idx) const;
     int rank  (const int idx, const int val) const;
-    int select(const int val, const int num) const;
+    int select(const int val, const int n) const;
     void print() const;
 };
 
